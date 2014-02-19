df9168ee652f0d51128fc46c98f214fc5978656e
Rick <ryan.elr.htc@gmail.com>
2014-02-18 22:17:36 -0500

    Remove gravatar code from user.js

--- app/models/user.js
+++ app/models/user.js
@@ -5,21 +5,22 @@ var User = DS.Model.extend({
 
     displayName: function() {
         return this.get('firstName') + ' ' + this.get('lastName');
-    }.property('firstName', 'lastName'),
+    }.property('firstName', 'lastName')
 
-    gravatarURL: function() {
-        var email = this.get('email'),
-            computedMD5;
-
-        if(Ember.isEmpty(email)) {
-            computedMD5 = '00000000000000000000000000000000';
-        } else {
-            computedMD5 = md5(email);
-        }
-
-        return 'http://www.gravatar.com/avatar/%@.jpg?s=80'.fmt(computedMD5);
-
-    }.property('email')
+//Gravatar related code is now in component and can be removed
+//    gravatarURL: function() {
+//        var email = this.get('email'),
+//            computedMD5;
+//
+//        if(Ember.isEmpty(email)) {
+//            computedMD5 = '00000000000000000000000000000000';
+//        } else {
+//            computedMD5 = md5(email);
+//        }
+//
+//        return 'http://www.gravatar.com/avatar/%@.jpg?s=80'.fmt(computedMD5);
+//
+//    }.property('email')
 });
 
 
